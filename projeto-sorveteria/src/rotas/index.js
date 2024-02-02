da99fc187f5bb94e6d5372abed3bcb824a9902ec
Renan Richa <renanricha@gmail.com>
2024-02-02 10:08:53 -0300

    Update index.js

--- projeto-sorveteria/src/rotas/index.js
+++ projeto-sorveteria/src/rotas/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HomePage from '../paginas/HomePage';
+import Sabores from '../paginas/Sabores'
+import Sobre from '../paginas/Sobre'
 
 const Rotas = () => (
     <div>
         <BrowserRouter>
-        <Routes>
-            <Route exact path="/" element={ <HomePage/> }/>
-            <Route path="/sabores" element={ <Sabores/> }/>
-            <Route path="/sobre" element={ <Sobre/> }/>
-        </Routes>
+            <Routes>
+                <Route exact path="/" element={ <HomePage/> }/>
+                <Route path="/sabores" element={ <Sabores/> }/>
+                <Route path="/sobre" element={ <Sobre/> }/>
+            </Routes>
         </BrowserRouter>
     </div>
     )
